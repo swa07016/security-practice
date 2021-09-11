47dc3ceac76912129f7ed35dcd67ca7fd37c7d99
swa07016 <swa07016@naver.com>
2021-09-11 15:44:17 +0900

    Add AuthenticationPrincipal annotation

--- src/main/java/com/security/demo/web/controller/DashboardController.java
+++ src/main/java/com/security/demo/web/controller/DashboardController.java
@@ -1,13 +1,18 @@
 package com.security.demo.web.controller;
 
+import com.security.demo.domain.User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashboardController {
 
     @GetMapping("/dashboard")
-    public String getDashboard() {
+    public String getDashboard(@AuthenticationPrincipal User user, Model model) {
+        System.out.println(user);
+        model.addAttribute("user", user);
         return "dashboard";
     }
 }
